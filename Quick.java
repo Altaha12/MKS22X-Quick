ff8b318f914a4bb16586fc5b4faadf41977713a4
Ali Taha <alitaha.1187@gmail.com>
2019-03-14 22:28:04 -0400

    just checked swap, it works

--- Quick.java
+++ Quick.java
@@ -7,19 +7,9 @@ public class Quick{
     ary[x1]=b;
     ary[x2]=a;
   }
-  private static int partition(int[] ary, int lo, int hi){
-    int pivot = (hi-lo)/2;
-    swap(ary,pivot,lo);
-    pivot=lo;
-    lo+=1;
-    while(lo<hi){
-      if(ary[lo]<=ary[pivot])lo+=1;
-      if(ary[lo]>ary[pivot]){
-        swap(ary,lo,hi);
-        hi-=1;}
-    }
-    swap(ary,pivot,lo);
-    return lo;
+  //just made dutch partition rather than other partitioning;
+  /*private static int[] partition(int[] ary, int lo, int hi){
+
   }
   public static void quicksort(int[] data){
     quicksortH(data,0,data.length-1);
@@ -29,13 +19,14 @@ public class Quick{
     int pivot = partition(data,lo,hi);
     quicksortH(data,lo,pivot-1);
     quicksortH(data,pivot+1,hi);
-  }
+  }*/
   public static void main(String[]args){
     int[] test = new int[]{5,5,3,2,1,8,9,6,34,1,5,3,4};
+    swap(test,0,2);
     System.out.println(Arrays.toString(test));
     //int r = partition(test,0,test.length-1);
     //System.out.println(r+Arrays.toString(test));
-    quicksort(test);
-    System.out.println(Arrays.toString(test));
+    //quicksort(test);
+    //System.out.println(Arrays.toString(test));
   }
 }
