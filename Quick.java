9900d71ad0c2fb3a5fb064163b1954559996d659
Ali Taha <alitaha.1187@gmail.com>
2019-03-14 23:37:42 -0400

    fixed partition and made a dutch partition method!

--- Quick.java
+++ Quick.java
@@ -7,11 +7,27 @@ public class Quick{
     ary[x1]=b;
     ary[x2]=a;
   }
-  //just made dutch partition rather than other partitioning;
-  /*private static int[] partition(int[] ary, int lo, int hi){
-
+  //just made dutch partition rather than other partitioning will do anyway;
+  private static int[] partition(int[] ary, int lo, int hi){
+    int pivot = ary[(hi-lo)/2];
+    System.out.println(pivot);
+    int bottom = lo;
+    int top = hi;
+    while(lo<=hi){
+      if(ary[lo]<pivot){lo++;}
+      else{swap(ary,lo,hi);hi-=1;}
+      System.out.println(Arrays.toString(ary));
+    }
+    hi++;
+    while(hi<top){
+      if(ary[hi]==pivot){hi++;}
+      else{swap(ary,hi,top);top-=1;}
+      System.out.println(Arrays.toString(ary));
+    }
+    int[] tbr = new int[]{lo-1,top+1};
+    return tbr;
   }
-  public static void quicksort(int[] data){
+  /*public static void quicksort(int[] data){
     quicksortH(data,0,data.length-1);
   }
   private static void quicksortH(int[] data, int lo, int hi){
@@ -22,10 +38,11 @@ public class Quick{
   }*/
   public static void main(String[]args){
     int[] test = new int[]{5,5,3,2,1,8,9,6,34,1,5,3,4};
-    swap(test,0,2);
+    int[] test1 = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12};
     System.out.println(Arrays.toString(test));
-    //int r = partition(test,0,test.length-1);
-    //System.out.println(r+Arrays.toString(test));
+    System.out.println(Arrays.toString(test1));
+    int[] r = partition(test,0,test.length-1);
+    System.out.println(Arrays.toString(r)+Arrays.toString(test));
     //quicksort(test);
     //System.out.println(Arrays.toString(test));
   }
