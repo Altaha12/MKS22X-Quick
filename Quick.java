46b1abf8b874e9789f5f646df6c73f514cd6e623
Ali Taha <alitaha.1187@gmail.com>
2019-03-14 23:56:02 -0400

    quicksort only works with odd length arrays so far

--- Quick.java
+++ Quick.java
@@ -39,7 +39,7 @@ public class Quick{
     quicksortH(data,pivot[1],hi);
   }
   public static void main(String[]args){
-    int[] test = new int[]{5,5,3,2,1,8,9,6,34,1,5,3,4};
+    int[] test = new int[]{5,5,3,2,1,8,9,6,34,1,5,3,4,468,625,12,32,3};
     //int[] test1 = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12};
     System.out.println(Arrays.toString(test));
     //System.out.println(Arrays.toString(test1));
@@ -48,4 +48,38 @@ public class Quick{
     quicksort(test);
     System.out.println(Arrays.toString(test));
   }
+  /*public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}**/
 }
